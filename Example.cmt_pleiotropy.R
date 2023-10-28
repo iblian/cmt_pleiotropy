8bfdc631c576269ca2132c8fb15dccecce7f6884
iblian <122533915+iblian@users.noreply.github.com>
2023-10-28 17:28:17 +0800

    Delete Example.cmt_pleiotropy.R

--- Example.cmt_pleiotropy.R
+++
@@ -1,8 +0,0 @@
-library(readxl)
-gout <- read_excel("C:/Users/User-Name/Downloads/gout.xlsx")
-result <- cmt_pleiotropy(outcome="gout",
-                         exposure="bmi",
-                         G=colnames(gout)[-c(1,12)],
-                         data=gout,
-                         Bootstrap_times = 100, prop = 0.8)
-result
